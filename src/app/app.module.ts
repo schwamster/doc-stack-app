d2eeb23f3d9a948369c33973c424515aceba6049
schwamster <bastian.toepfer@bodoconsult.de>
2016-11-03 07:05:02 +0100

    removed all .net core parts changed ng2 proj to a angular-cli project

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { AppRoutingModule } from './app.routing.module'
+
+import { UPLOAD_DIRECTIVES } from 'ng2-uploader/ng2-uploader';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { MapIteratorPipe } from './shared';
+
+import '../styles.scss';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MapIteratorPipe,
+    UPLOAD_DIRECTIVES
+  ],
+  imports: [
+    Ng2BootstrapModule,
+    AlertModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
